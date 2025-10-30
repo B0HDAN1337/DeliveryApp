9d07114767a0501d5ae50d19e77f642b97b1c3d2
B0HDAN1337 <machuliak1337@gmail.com>
2025-10-30 02:08:14 +0100

    init simple navigationBar

---
+++ lib/navigation.dart
@@ -0,0 +1,54 @@
+import 'package:delivery_app/pages/home_page.dart';
+import 'package:delivery_app/pages/second_page.dart';
+import 'package:delivery_app/pages/third_page.dart';
+import 'package:flutter/material.dart';
+
+class Navigation extends StatefulWidget {
+  const Navigation({super.key});
+
+  @override
+  State<Navigation> createState() => _NavigationState();
+}
+
+class _NavigationState extends State<Navigation> {
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+
+    final List<Widget> pages = const [
+      HomePage(),
+      SecondPage(),
+      ThirdPage(),
+    ];
+
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: Colors.amber,
+        selectedIndex: currentPageIndex,
+        destinations: const[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Badge(child: Icon(Icons.home)),
+            label: 'second',
+          ),
+          NavigationDestination(
+            icon: Badge(child: Icon(Icons.home)),
+            label: 'third',
+          ),
+        ],
+      ),
+      body: pages [currentPageIndex],
+    );
+  }
+}
---
+++ lib/pages/second_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Card(
+      shadowColor: Colors.transparent,
+      margin: const EdgeInsets.all(8.0),
+      child: SizedBox.expand(
+        child: Center(child: Text('Second page', style: theme.textTheme.titleLarge)),
+      ),
+    );
+  }
+}
